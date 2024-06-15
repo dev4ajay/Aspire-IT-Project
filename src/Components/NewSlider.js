b476eaabd0aea7067b665e3cca80aab66b67be91
dev4ajay <dev4ajaykumar@gmail.com>
2024-06-15 18:36:21 +0530

    main

--- src/Components/NewSlider.js
+++ src/Components/NewSlider.js
@@ -38,7 +38,7 @@ return(
 
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span   aria-hidden="true"><img src={require("../Assets/image/download arrow right-PhotoRoom.png-PhotoRoom.png")} style={{width:'36px' }} className="arrow_btn"/></span>
+    <span   aria-hidden="true"><img src={require("../Assets/image/download arrow right-PhotoRoom.png-PhotoRoom.png")} style={{width:'28px' }} className="arrow_btn"/></span>
     <span class="visually-hidden">Previous</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
